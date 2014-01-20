be35d0d004a25efe57be6e83bf64c4c82f8b3228
mdbitz <matt@mdbitz.com>
2014-01-20 04:09:45 +0000

    Separated tooltip content into individual files by slug. Added some tooltips to the Posts page.

---
+++ docs/edit-post-tips.php
@@ -0,0 +1,7 @@
+<?php
+
+$this->tooltip_help_content['filtercategory'] = '<p>' . __( 'Use the dropdown menus to the left to refine the list of posts by a specific category or from a specific month by. Click the Filter button after making your selection.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['publishfilter'] = '<p>' . __( 'Click here to filter the list of posts shown to only include published posts.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['allfilter'] = '<p>' . __( 'Click here to display all posts regardless of current status.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['draftfilter'] = '<p>' . __( 'Click here to filter the list of posts shown to only include posts in draft status.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['trashfilter'] = '<p>' . __( 'Click here to filter the list of posts shown to only those that have been deleted.', 'ah-o2' ) . '</p>';
---
+++ js/adminhelp-edit-post.js
@@ -0,0 +1,46 @@
+jQuery(function( $ ) {
+	/**
+	 * Setup tooltips as objects in an array.
+	 * @param string slug How the AdminHelp class identifies the tooltip
+	 * @param array selector Array of css selectors for the items the tooltip
+	 * should be attached to
+	 * @param string content Content to be displayed in the tooltip
+	 */
+	adminHelp.add([
+		{
+			slug: 'filtercategory'
+			,selector: [
+				'#post-query-submit'
+			],
+			content: adminhelp_content.filtercategory
+		},
+		{
+			slug: 'publishfilter'
+			,selector: [
+				'.publish'
+			],
+			content: adminhelp_content.publishfilter
+		},
+		{
+			slug: 'allfilter'
+			,selector: [
+				'.all'
+			],
+			content: adminhelp_content.allfilter
+		},
+		{
+			slug: 'draftfilter'
+			,selector: [
+				'.draft'
+			],
+			content: adminhelp_content.draftfilter
+		},
+		{
+			slug: 'trashfilter'
+			,selector: [
+				'.trash'
+			],
+			content: adminhelp_content.trashfilter
+		}
+	]);
+});
