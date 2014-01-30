2d1815740559ed9ba1ffa208da84128389ba637f
jazzsequence <chris@jazzsequence.com>
2014-01-29 19:41:54 -0700

    adds a tooltip to the add new users button
    MY VERY FIRST TOOLTIP OMGWTFBBQ!!!!

---
+++ docs/users-tips.php
@@ -0,0 +1,15 @@
+<?php
+
+$this->tooltip_help_content['addnew'] = '<p>' . __( 'Add a new user to your site.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['descriptioncolumn'] = '<p>' . __( 'Descriptions of currently available plugins', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['activatebutton'] = '<p>' . __( 'Activate this plugin', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['deactivatebutton'] = '<p>' . __( 'Deactivate this plugin', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['editbutton'] = '<p>' . __( 'Open this plugin in the Plugin Editor', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['settingsbutton'] = '<p>' . __( 'Configure the settings of this plugin', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['deletebutton'] = '<p>' . __( 'Delete this plugin', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['alltab'] = '<p>' . __( 'View all available plugins', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['activetab'] = '<p>' . __( 'View all active plugins', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['inactivetab'] = '<p>' . __( 'View all inactive plugins', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['updatetab'] = '<p>' . __( 'View all plugins with available updates', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['mustusetab'] = '<p>' . __( 'View all must use plugins', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['dropinstab'] = '<p>' . __( 'View drop in plugins', 'ah-o2' ) . '</p>';
---
+++ js/adminhelp-users.js
@@ -0,0 +1,102 @@
+jQuery(function( $ ) {
+	/**
+	 * Setup tooltips as objects in an array.
+	 * @param string slug How the AdminHelp class identifies the tooltip
+	 * @param array selector Array of css selectors for the items the tooltip
+	 * should be attached to
+	 * @param string content Content to be displayed in the tooltip
+	 */
+	adminHelp.add([
+		{
+			slug: 'addnew',
+			selector: [
+				'.add-new-h2'
+			],
+			content: adminhelp_content.addnew
+		},
+		{
+			slug: 'descriptioncolumn',
+			selector: [
+				'.manage-column.column-description'
+			],
+			content: adminhelp_content.descriptioncolumn
+		},
+		{
+			slug: 'activatebutton',
+			selector: [
+				'span.activate'
+			],
+			content: adminhelp_content.activatebutton
+		},
+		{
+			slug: 'deactivatebutton',
+			selector: [
+				'span.deactivate'
+			],
+			content: adminhelp_content.deactivatebutton
+		},
+		{
+			slug: 'editbutton',
+			selector: [
+				'span.edit'
+			],
+			content: adminhelp_content.editbutton
+		},
+		{
+			slug: 'settingsbutton',
+			selector: [
+				'span.settings'
+			],
+			content: adminhelp_content.settingsbutton
+		},
+		{
+			slug: 'deletebutton',
+			selector: [
+				'span.delete'
+			],
+			content: adminhelp_content.deletebutton
+		},
+		{
+			slug: 'alltab',
+			selector: [
+				'.subsubsub .all'
+			],
+			content: adminhelp_content.alltab
+		},
+		{
+			slug: 'activetab',
+			selector: [
+				'.subsubsub .active'
+			],
+			content: adminhelp_content.activetab
+		},
+		{
+			slug: 'inactivetab',
+			selector: [
+				'.subsubsub .inactive'
+			],
+			content: adminhelp_content.inactivetab
+		},
+		{
+			slug: 'updatetab',
+			selector: [
+				'.subsubsub .update'
+			],
+			content: adminhelp_content.updatetab
+		},
+		{
+			slug: 'mustusetab',
+			selector: [
+				'.subsubsub .mustuse'
+			],
+			content: adminhelp_content.mustusetab
+		},
+		{
+			slug: 'dropinstab',
+			selector: [
+				'.subsubsub .dropins'
+			],
+			content: adminhelp_content.dropinstab
+		}
+	]);
+});
