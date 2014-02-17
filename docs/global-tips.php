76490c4510662958f82c8bb2b31cedce77da2a80
Chris Reynolds <chris@jazzsequence.com>
2014-02-17 10:53:20 -0700

    updates global tips. creates more descriptive tooltips.

--- docs/global-tips.php
+++ docs/global-tips.php
@@ -1,13 +1,14 @@
 <?php
 
-$this->tooltip_help_content['dashboardnav'] = '<p>' . __( 'Main admin screen', 'ah-o2' ) . '</p>';
-$this->tooltip_help_content['postsnav'] = '<p>' . __( 'Posts admin screen', 'ah-o2' ) . '</p>';
-$this->tooltip_help_content['medianav'] = '<p>' . __( 'Media admin screen', 'ah-o2' ) . '</p>';
-$this->tooltip_help_content['pagesnav'] = '<p>' . __( 'Pages', 'ah-o2' ) . '</p>';
-$this->tooltip_help_content['commentsnav'] = '<p>' . __( 'Comments admin screen', 'ah-o2' ) . '</p>';
-$this->tooltip_help_content['appearancenav'] = '<p>' . __( 'Appearance admin screen', 'ah-o2' ) . '</p>';
-$this->tooltip_help_content['pluginsnav'] = '<p>' . __( 'Plugins admin screen', 'ah-o2' ) . '</p>';
-$this->tooltip_help_content['usersnav'] = '<p>' . __( 'Users admin screen', 'ah-o2' ) . '</p>';
-$this->tooltip_help_content['toolsnav'] = '<p>' . __( 'Tools admin screen', 'ah-o2' ) . '</p>';
-$this->tooltip_help_content['settingsnav'] = '<p>' . __( 'Settings admin screen', 'ah-o2' ) . '</p>';
-$this->tooltip_help_content['collapsenav'] = '<p>' . __( 'Collapse/expand menu', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['dashboardnav'] = '<p>' . __( 'You can get an overview of your site from here. Within this menu, you may access WordPress updates.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['postsnav'] = '<p>' . __( 'Start here to compose blog posts and updates for your site.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['medianav'] = '<p>' . __( 'This is where you can access and upload any images, movies, music or other files on your site.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['linksnav'] = '<p>' . __( 'This page is used to access and add to your links (formerly known as Blogroll).' );
+$this->tooltip_help_content['pagesnav'] = '<p>' . __( 'Create static content that is not part of the chronological blog stream.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['commentsnav'] = '<p>' . __( 'Review and approve, trash or flag as spam responses left on posts to your site.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['appearancenav'] = '<p>' . __( 'Change the design of your site by selecting a new theme or customize the existing theme. If your current theme supports it, you may also create and edit menus, custom backgrounds or custom headers.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['pluginsnav'] = '<p>' . __( 'Add new functionality to your site by installing and activating plugins. Remove this functionality by deactivating and deleting plugins.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['usersnav'] = '<p>' . __( 'Create new users to grant others access to your WordPress admin.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['toolsnav'] = '<p>' . __( 'A collection of utilities to help you manage your content.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['settingsnav'] = '<p>' . __( 'Edit and manage the options that determine how your site is set up.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['collapsenav'] = '<p>' . __( 'Use this to either shrink the sidebar to only display icons for each menu or expand the sidebar from collapsed view.', 'ah-o2' ) . '</p>';
