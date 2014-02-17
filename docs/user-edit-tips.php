9c319617e32cf16c76bddd9f178eca928f72af6b
Chris Reynolds <chris@jazzsequence.com>
2014-02-17 16:46:47 -0700

    adds edit user tooltips

---
+++ docs/user-edit-tips.php
@@ -0,0 +1,23 @@
+<?php
+$this->tooltip_help_content['richediting'] = '<p>' . __( 'Check this to disable the WYSIWYG editor in edit screens.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['coloroptions'] = '<p>' . __( 'Change the look and feel of the WordPress admin by selecting one of these color schemes.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['commentshortcuts'] = '<p>' . __( 'Keyboard shortcuts are enabled on a per-user setting. These keyboard shortcuts are designed to save time by allowing you to rapidly navigate and perform actions on comments.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['showadminbar'] = '<p>' . __( 'Check or uncheck this box to define whether to display the admin bar when logged in and viewing your site. Default is to display the admin bar.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['showhelp'] = '<p>' . __( 'Settings to control the way help is displayed in the WordPress admin.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['helptooltips'] = '<p>' . __( 'Show or hide these help tooltips.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['helpoverview'] = '<p>' . __( 'Show or hide help overview boxes at the top of the page. Help boxes can be displayed at any time by clicking the help toggle if "Always show help overviews" is disabled.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['username'] = '<p>' . __( 'The username for this user. This field is disabled; usernames cannot be modified.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['firstname'] = '<p>' . __( 'The user\'s first name.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['lastname'] = '<p>' . __( 'The user\'s last name.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['nickname'] = '<p>' . __( 'The user\'s nickname (if different than first name or username).', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['nicename'] = '<p>' . __( 'How this user\'s name is displayed on the front and back end.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['email'] = '<p>' . __( 'The user\'s email address.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['website'] = '<p>' . __( 'The user\'s home page URL.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['aim'] = '<p>' . __( 'The user\'s AOL Instant Messenger ID.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['yim'] = '<p>' . __( 'The user\'s Yahoo! Instant Messenger ID.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['jabber'] = '<p>' . __( 'The user\'s Jabber/Google ID.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['bio'] = '<p>' . __( 'A description or bio for this user.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['newpass1'] = '<p>' . __( 'Enter a new password for this user. Leave this blank unless you want to change the password.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['newpass2'] = '<p>' . __( 'Repeat the new password for this user.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['passwordstrength'] = '<p>' . __( 'Displays the strength of your password. Strong passwords are longer and may include a combination of letters, numbers and special characters.', 'ah-o2' ) . '</p>';
+$this->tooltip_help_content['update'] = '<p>' . __( 'Save changes to this user.', 'ah-o2' ) . '</p>';
